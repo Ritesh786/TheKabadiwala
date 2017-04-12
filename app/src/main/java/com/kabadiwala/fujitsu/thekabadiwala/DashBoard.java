0b92ae186671760e6ae698c029fc227e6c60f3e2
Ritesh Jha <jharitesh6@gmail.com>
2017-04-12 18:21:43 +0530

    Set profile Picture And Email

--- app/src/main/java/com/kabadiwala/fujitsu/thekabadiwala/DashBoard.java
+++ app/src/main/java/com/kabadiwala/fujitsu/thekabadiwala/DashBoard.java
@@ -3,21 +3,31 @@ package com.kabadiwala.fujitsu.thekabadiwala;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
+import com.facebook.Profile;
 import com.facebook.login.LoginManager;
+import com.squareup.picasso.Picasso;
 
 public class DashBoard extends AppCompatActivity implements View.OnClickListener {
 
     Button mlgoutbtn;
+
+    TextView memailtext;
+    ImageView mfbimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,47 +39,43 @@ public class DashBoard extends AppCompatActivity implements View.OnClickListener
 
         mlgoutbtn = (Button) findViewById(R.id.lgout_btn);
         mlgoutbtn.setOnClickListener(this);
+
+        memailtext = (TextView) findViewById(R.id.email_text);
+        mfbimage = (ImageView) findViewById(R.id.fb_image);
+
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("email");
+
+
+
+        memailtext.setText(email);
+
+        Bundle mBundle =intent.getExtras();
+ //         Profile profile = mBundle.getParcelable("pic");
+
+        Profile  profile;
+
+        if (mBundle != null) {
+           profile = (Profile) mBundle.getParcelable("pic");
+        } else {
+            profile = Profile.getCurrentProfile();
+        }
+
+
+        Picasso.with(DashBoard.this)
+                .load(profile.getProfilePictureUri(400, 400).toString())
+                .into(mfbimage);
+
+
+
     }
 
     @Override
     public void onClick(View v) {
 
         disconnectFromFacebook();
-    }
 
-//    private void logoutFromFacebook(){
-//
-//        final SharedPreferences pref = DashBoard.this.getPreferences(Context.MODE_PRIVATE);
-//        String id = pref.getString("facebook_id", "empty");
-//
-//        try {
-//            if (AccessToken.getCurrentAccessToken() == null) {
-//                return; // already logged out
-//            }
-//             //get fb id from sharedprefrences
-//            GraphRequest graphRequest=new GraphRequest(AccessToken.getCurrentAccessToken(), "/ "+id+"/permissions/", null,
-//                    HttpMethod.DELETE, new GraphRequest.Callback() {
-//                @Override
-//                public void onCompleted(GraphResponse graphResponse) {
-//                    LoginManager.getInstance().logOut();
-//
-//                    SharedPreferences.Editor editor = pref.edit();
-//                    editor.remove("facebook_id");
-//                    editor.commit();
-//                    finish();
-//
-//                    Intent logoutint = new Intent(DashBoard.this,MainActivity.class);
-//                    startActivity(logoutint);
-//                }
-//            });
-//
-//            graphRequest.executeAsync();
-//        }catch(Exception ex) {
-//            ex.printStackTrace();
-//        }
-//
-//
-//    }
+    }
 
 
     public void disconnectFromFacebook() {
@@ -94,6 +100,8 @@ public class DashBoard extends AppCompatActivity implements View.OnClickListener
 
             }
         }).executeAsync();
+
+
     }
 
 }
--- app/src/main/java/com/kabadiwala/fujitsu/thekabadiwala/MainActivity.java
+++ app/src/main/java/com/kabadiwala/fujitsu/thekabadiwala/MainActivity.java
@@ -1,8 +1,11 @@
 package com.kabadiwala.fujitsu.thekabadiwala;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,9 +21,11 @@ import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphRequestAsyncTask;
 import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
@@ -35,7 +40,10 @@ public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
 
+    ProgressDialog progress;
+    private String facebook_id,f_name, m_name, l_name, gender, profile_image, full_name, email_id;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -50,19 +58,20 @@ public class MainActivity extends AppCompatActivity {
 
                         SharedPreferences pref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
 
-                            GraphRequestAsyncTask request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),new GraphRequest.GraphJSONObjectCallback(){
+//                            GraphRequestAsyncTask request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),new GraphRequest.GraphJSONObjectCallback(){
+//
+//                                @Override
+//                                public void onCompleted(JSONObject object, GraphResponse response) {
+//
+//
+//                                    Toast.makeText(getApplicationContext(),object.toString(),Toast.LENGTH_LONG).show();
+//
+//                                }
+//                            }).executeAsync();
 
-                                @Override
-                                public void onCompleted(JSONObject object, GraphResponse response) {
+                        graphRequest(loginResult.getAccessToken());
 
 
-                                    Toast.makeText(getApplicationContext(),object.toString(),Toast.LENGTH_LONG).show();
-
-                                }
-                            }).executeAsync();
-
-                        Intent loginint = new Intent(MainActivity.this,DashBoard.class);
-                        startActivity(loginint);
 
                     }
 
@@ -81,6 +90,11 @@ public class MainActivity extends AppCompatActivity {
 
         fbloginbtn = (Button)findViewById(R.id.fblogin_btn);
 
+        progress=new ProgressDialog(MainActivity.this);
+        progress.setMessage(getResources().getString(R.string.please_wait_facebooklogin));
+        progress.setIndeterminate(false);
+        progress.setCancelable(false);
+
         fbloginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -97,5 +111,59 @@ public class MainActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
+    public void graphRequest(AccessToken token){
+
+
+
+        GraphRequest request = GraphRequest.newMeRequest(token,new GraphRequest.GraphJSONObjectCallback(){
+
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                JSONObject me = response.getJSONObject();
+
+                if (me != null) {
+
+                    try {
+
+                        Profile profile = Profile.getCurrentProfile();
+
+
+                        Bundle mBundle = new Bundle();
+                               mBundle.putParcelable("pic", profile);
+
+
+                        String email = object.getString("email");
+                        String id = object.optString("id");
+                        String name = object.optString("name");
+
+
+                        Intent loginint = new Intent(MainActivity.this,DashBoard.class);
+                       loginint.putExtra("email",email);
+                        loginint.putExtras(mBundle);
+
+
+
+                        startActivity(loginint);
+                        finish();
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                Toast.makeText(getApplicationContext(),object.toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        Bundle b = new Bundle();
+        b.putString("fields","id,email,first_name,last_name,picture.type(large)");
+        request.setParameters(b);
+        request.executeAsync();
+
     }
+
+}
 
